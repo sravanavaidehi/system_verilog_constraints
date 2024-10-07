3ab2c1182ae766bb95c4e5dd3a3f0bb6b5364dc6
sravanavaidehi <sravanavaidehi@gmail.com>
2024-10-07 15:17:14 -0500

    Update constraint to generate 10-bit variable with alternative number.sv
    constraint to generate 10-bit variable with alternative number (1010101010)

--- constraint to generate 10-bit variable with alternative number.sv
+++ constraint to generate 10-bit variable with alternative number.sv
@@ -1,1 +1,20 @@
-
+class ex;
+  rand bit  a[10];
+  
+  constraint pattern{ foreach(a[i]){
+    i == 0 -> a[i] == 1;
+    i>0 -> a[i] != a[i-1];
+  }
+                    }
+endclass
+               
+module tb;
+  initial begin
+   //repeat(10) begin
+    ex e = new();
+    e.randomize();
+     $write("\n pattern : %p", e.a);
+  // end
+    
+  end
+endmodule
