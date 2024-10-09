b8ddaf4ceb3d9dd6d2b4f6dfc9d42d03c76154f5
sravanavaidehi <sravanavaidehi@gmail.com>
2024-10-09 13:45:21 -0500

    Create constraint for a 4 variable is not same as the last 5 occurrence.sv

---
+++ constraint for a 4 variable is not same as the last 5 occurrence.sv
@@ -0,0 +1,25 @@
+class ex;
+  rand bit [3:0] a;
+  int que[$:7];
+  constraint pattern{!(a inside{que});}
+  function void post_randomize();
+    que.push_front(a);
+    $display("\n pattern a : %h",a);
+    if(que.size == 6)
+        que.pop_back();
+     $display("\n previous 5 occurance  : %p",que);
+ 
+  endfunction
+                            
+endclass
+    
+               
+module tb;
+  initial begin
+     ex e = new();
+  repeat(11) begin
+    e.randomize();
+ end
+   
+  end
+endmodule     
